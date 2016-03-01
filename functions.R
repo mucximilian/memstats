6360da5fdeca3a41caee6fee4b624f1b5fe59df9
mucximilian <mucximilian@gmail.com>
2016-03-01 14:57:06 +0100

    New function outline added, not working yet

--- functions.R
+++ functions.R
@@ -106,21 +106,200 @@ get_per_day <- function(stats, label) {
 
 ################################################################################
 
+get_cum <- function(stats, label) {
+    plot_daily_graph(stats[,c(1,2)], paste(label, "cum", sep="/")) # Points
+    plot_daily_graph(stats[,c(1,4)], paste(label, "cum", sep="/")) # Items
+}
+
+get_abs <- function(stats, label) {
+    plot_daily_scatterplot(stats, label)
+}
+
 
 get_total <- function(stats) {
-    plot_daily_graph(stats[,c(1,2)], "total/cum") # Points cumulative
-    plot_daily_graph(stats[,c(1,4)], "total/cum") # Items cumulative
+    
+    abs_points <- stats[,c(1,3)]
+    abs_items <- stats[,c(1,5)]
+    
+    ############################################################################
+    # Cumulative per day
+    get_cum(stats[,c(1,2,4)], "total")
+    
+    # Absolute per day
+    get_abs(abs_points, "total")
+    get_abs(abs_items, "total")
+    
+    # Daily means
+    get_mean_daily_week(abs_points, "total")
+    get_mean_daily_month(abs_points, "total")
+    get_mean_daily_quarter(abs_points, "total")
+    get_mean_daily_year(abs_points, "total")
+    
+    get_mean_daily_week(abs_items, "total")
+    get_mean_daily_month(abs_items, "total")
+    get_mean_daily_quarter(abs_items, "total")
+    get_mean_daily_year(abs_items, "total")
+    
+    mean_daily_points <- get_mean(abs_points)
+    mean_daily_items <- get_mean(abs_items)
+    
+    ############################################################################
+    # Week sums
+    sum_weekly_points <- get_sum_weekly(abs_points, "total")
+    sum_weekly_items <- get_sum_weekly(abs_items, "total")
+    
+    # Weekly means
+    get_mean_weekly_month(sum_weekly_points, "total")
+    get_mean_weekly_quarter(sum_weekly_points, "total")
+    get_mean_weekly_year(sum_weekly_points, "total")
+    
+    get_mean_weekly_month(sum_weekly_items, "total")
+    get_mean_weekly_quarter(sum_weekly_items, "total")
+    get_mean_weekly_year(sum_weekly_items, "total")
+    
+    mean_weekly_points <- get_mean(sum_weekly_points)
+    mean_weekly_items <- get_mean(sum_weekly_items)
+    
+    ############################################################################
+    # Month sums
+    sum_monthly_points <- get_sum_monthly(abs_points, "total")
+    sum_monthly_items <- get_sum_monthly(abs_items, "total")
+    
+    # Monthly means
+    get_mean_monthly_quarter(sum_monthly_points, "total")
+    get_mean_monthly_year(sum_monthly_points, "total")
+    
+    get_mean_monthl_quarter(sum_monthly_items, "total")
+    get_mean_monthly_year(sum_monthly_items, "total")
+    
+    mean_monthly_points <- get_mean(sum_monthly_points)
+    mean_monthly_items <- get_mean(sum_monthly_items)
+    
+    ############################################################################
+    # Quarter sums
+    sum_quarterly_points <- get_sum_quarterly(abs_points, "total")
+    sum_quarterly_items <- get_sum_quarterly(abs_items, "total")
+    
+    # Quarterly means
+    get_mean_quarterly_year(sum_quarterly_points, "total")
+    
+    get_mean_quarterly_year(sum_quarterly_items, "total")
+    
+    mean_quarterly_points <- get_mean(sum_quarterly_points)
+    mean_quarterly_items <- get_mean(sum_quarterly_items)
+    
+    ############################################################################
+    # Year sums
+    sum_yearly_points <- get_sum_yearly(abs_points, "total")
+    sum_yearly_points <- get_sum_yearly(abs_items, "total")
+    
+    # Year means
+    mean_yearly_points <- get_mean(sum_yearly_points, "total")
+    mean_yearly_items <- get_mean(sum_yearly_points, "total")
+}
 
-    get_stats_total(stats[,c(1,3)], sum, "total/abs_sum")
-    get_stats_total(stats[,c(1,5)], sum, "total/abs_sum")
-    get_stats_total(stats[,c(1,3)], mean, "total/daily_mean")
-    get_stats_total(stats[,c(1,5)], mean, "total/daily_mean")
+get_year <- function(stats) {
+    
+    abs_points <- stats[,c(1,3)]
+    abs_items <- stats[,c(1,5)]
+    
+    ############################################################################
+    # Cumulative per day
+    get_cum(stats[,c(1,2,4)], "year")
+    
+    # Absolute per day
+    get_abs(abs_points, "year")
+    get_abs(abs_items, "year")
+    
+    # Daily means
+    get_mean_daily_week(abs_points, "year")
+    get_mean_daily_month(abs_points, "year")
+    get_mean_daily_quarter(abs_points, "year")
+    
+    get_mean_daily_week(abs_items, "year")
+    get_mean_daily_month(abs_items, "year")
+    get_mean_daily_quarter(abs_items, "year")
+    
+    ############################################################################
+    # Week sums
+    sum_weekly_points <- get_sum_weekly(abs_points, "year")
+    sum_weekly_items <- get_sum_weekly(abs_items, "year")
+    
+    # Weekly means
+    get_mean_weekly_month(sum_weekly_points, "year")
+    get_mean_weekly_quarter(sum_weekly_points, "year")
+    
+    get_mean_weekly_month(sum_weekly_items, "year")
+    get_mean_weekly_quarter(sum_weekly_items, "year")
+    
+    ############################################################################
+    # Month sums
+    sum_monthly_points <- get_sum_monthly(abs_points, "year")
+    sum_monthly_items <- get_sum_monthly(abs_items, "year")
+    
+    # Monthly means
+    get_mean_monthly_quarter(sum_monthly_points, "year")
+    
+    get_mean_monthl_quarter(sum_monthly_items, "year")
+    
+    ############################################################################
+    # Quarter sums
+    sum_quarterly_points <- get_sum_quarterly(abs_points, "year")
+    sum_quarterly_items <- get_sum_quarterly(abs_items, "year")
+}
+
+get_month <- function(stats) {
+    
+    abs_points <- stats[,c(1,3)]
+    abs_items <- stats[,c(1,5)]
+    
+    ############################################################################
+    # Cumulative per day
+    get_cum(stats[,c(1,2,4)], "year")
+    
+    # Absolute per day
+    get_abs(abs_points, "year")
+    get_abs(abs_items, "year")
+    
+    # Daily means
+    get_mean_daily_week(abs_points, "year")
+    
+    get_mean_daily_week(abs_items, "year")
+    
+    ############################################################################
+    # Week sums
+    sum_weekly_points <- get_sum_weekly(abs_points, "year")
+    sum_weekly_items <- get_sum_weekly(abs_items, "year")
+}
+
+get_week <- function(stats) {
+    
+    abs_points <- stats[,c(1,3)]
+    abs_items <- stats[,c(1,5)]
+    
+    ############################################################################
+    # Cumulative per day
+    get_cum(stats[,c(1,2,4)], "year")
+    
+    # Absolute per day
+    get_abs(abs_points, "year")
+    get_abs(abs_items, "year")
 }
 
 split_by_year <- function(stats) {
     stats.split <- get_period_splits(stats, "year")
     
-    lapply(stats.split, plot_cum)
-    lapply(stats.split, get_stats_year, fun=sum)
-    lapply(stats.split, get_stats_year, fun=mean)
+    get_year(stats)
+}
+
+split_by_month <- function(stats) {
+    stats.split <- get_period_splits(stats, "month")
+    
+    get_month(stats)
+}
+
+split_by_year <- function(stats) {
+    stats.split <- get_period_splits(stats, "week")
+    
+    get_week(stats)
 }
