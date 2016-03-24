393d420eecb2e2477b6e4cd74c645c1b7b104897
mucximilian <mucximilian@gmail.com>
2016-03-24 15:25:29 +0100

    English plot labels for dates ensured

--- setup.R
+++ setup.R
@@ -7,3 +7,6 @@ library(plyr)
 source("functions.R")
 source("functions_evaluation.R")
 source("functions_plot.R")
+
+# Ensure english plot labels for the time
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
